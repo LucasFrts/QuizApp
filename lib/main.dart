4382b710c1e968d0aaab919f1d753f9c8b5d9ae4
Lucas Freitas <lmrfreitas9@gmail.com>
2023-04-02 11:43:42 -0300

    Inicio Projeto

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(new QuizApp());
+}
+class QuizApp extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      home: Text('Olá Mundo'),
+
+    );
+  }
+}
